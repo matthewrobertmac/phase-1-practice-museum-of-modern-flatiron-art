e5204e10977e9a866ea7c5c2032e7c7cb309de15
+ <+>
2023-03-29 14:17:39 -0400

    Deliverables 1 & 2 complete!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
+let commentsSection = document.getElementById("comments-section")
+let form = document.getElementById("comment-form")
+
+
 fetch("http://localhost:3000/current-exhibits")
   .then((response) => response.json())
-  .then((data) => populateData(data[0]))
+  .then((data) => {
+    firstData = (data[0]),
+    addComments(firstData),
+    populateData(data[0])
+  })
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault(),
+  console.log(e.target["comment-input"].value),
+  newComment = document.createElement("p")
+  newComment.textContent = e.target["comment-input"].value
+  commentsSection.append(newComment)
+  // commentsSection.append(`\n \t${newComment}\n`)
+})
 
 function populateData(data) {
   console.log(data),
@@ -11,4 +28,13 @@ function populateData(data) {
   exhibitImage = document.getElementById("exhibit-image")
   exhibitImage.src = data.image
 
+}
+
+function addComments(firstData) {
+  let commentsArray = firstData.comments
+  commentsArray.forEach((comment) => {
+    oldComment = document.createElement("p")
+    oldComment.textContent = comment
+    commentsSection.append(oldComment)
+  })
 }
