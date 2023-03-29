4d2c3b04a9cdeaf4d9ae767190144a3bb6931d1e
+ <+>
2023-03-28 21:29:26 -0400

    good start!

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('Write your code here');
+fetch("http://localhost:3000/current-exhibits")
+  .then((response) => response.json())
+  .then((data) => populateData(data[0]))
+
+function populateData(data) {
+  console.log(data),
+  exhibitTitle = document.getElementById("exhibit-title")
+  exhibitTitle.textContent = data.title
+  exhibitDescription = document.getElementById("exhibit-description")
+  exhibitDescription.textContent = data.description
+  exhibitImage = document.getElementById("exhibit-image")
+  exhibitImage.src = data.image
+
+}
